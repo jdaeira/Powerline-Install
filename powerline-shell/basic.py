e09783503371052bbe9e86470d062e18a8d1195c
John da Eira <jdaeira@gmail.com>
2018-08-01 14:13:08 -0700

    Changed the colors of the basic.py file

--- powerline-shell/basic.py
+++ powerline-shell/basic.py
@@ -3,15 +3,15 @@ from powerline_shell.themes.default import DefaultColor
 
 class Color(DefaultColor):
     """Basic theme which only uses colors in 0-15 range"""
-    USERNAME_FG = 15
-    USERNAME_BG = 12
+    USERNAME_FG = 8
+    USERNAME_BG = 15
     USERNAME_ROOT_BG = 1
 
-    HOSTNAME_FG = 15	
-    HOSTNAME_BG = 27
+    HOSTNAME_FG = 8
+    HOSTNAME_BG = 7
 
     HOME_SPECIAL_DISPLAY = False
-    PATH_BG = 33  # dark grey
+    PATH_BG = 8  # dark grey
     PATH_FG = 7  # light grey
     CWD_FG = 15  # white
     SEPARATOR_FG = 7
